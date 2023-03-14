89113ee98bba3e6bd66f885e5f77bab16c127a85
Iuliia Goriun <goriunyuliya@gmail.com>
2023-03-14 12:44:32 +0300

    Adds main project markup

---
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -0,0 +1,40 @@
+import DiaryAddProductForm from 'components/DiaryAddProductForm/DiaryAddProductForm';
+import RightSideBar from 'components/RightSideBar/RightSideBar';
+import css from './DiaryPage.module.scss';
+
+import React, { useState } from 'react';
+import { ModalMobAddGrams } from 'components/ModalMobAddGrams/ModalMobAddGrams';
+import { useSelector } from 'react-redux';
+import { selectiIsLoadingUser } from 'redux/auth/authSelectors';
+import { Loader } from 'components/Loader/Loader';
+import { selectisLoadingProduct } from 'redux/dayCalendar/dayCalendarSelectors';
+
+export default function DiaryPage() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const isLoadingUser = useSelector(selectiIsLoadingUser);
+  const isLoadingProduct = useSelector(selectisLoadingProduct);
+
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+    document.body.classList.add('noScrool');
+  };
+
+  const onClose = () => {
+    setIsOpenModal(false);
+    document.body.classList.remove('noScrool');
+  };
+
+  return (
+    <div className={css.diaryPage}>
+      {(isLoadingProduct || isLoadingUser) && <Loader />}
+
+      <DiaryAddProductForm handleOpenModal={handleOpenModal} />
+
+      <RightSideBar />
+
+      {isOpenModal && (
+        <ModalMobAddGrams onClose={onClose} isOpenModal={isOpenModal} />
+      )}
+    </div>
+  );
+}
